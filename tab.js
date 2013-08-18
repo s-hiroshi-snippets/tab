ab75fbe6091d225ea3496499022177655cf61354
s_hiroshi <info@info-town.jp>
2013-08-18 16:23:28 +0900

    Update tab.js

--- tab.js
+++ tab.js
@@ -1,27 +1,57 @@
 /**
  * tab menu
+ *
+ * load style/script in head
+ * ```html
+ * <link rel="stylesheet" href="style.css" media="all">
+ * <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+ * <script src="tab.js"></script>
+ * <script>
+ * jQuery(function($) {
+ *       $( '#tab' ).tab();
+ *   })
+ * </script>
+ * ```
+ *
+ *
+ * markup tab menu
+ *
+ *```html
+ *<div class="tab">
+ *<ul class="tab-menu">
+ *<li><a href="#" class="active">tab1</ul>
+ *<li><a href="#">tab2</ul>
+ *</ul>
+ *<div class="tab-content">
+ *statements
+ *</div>
+ *<div class="tab-content">
+ *statements
+ *</div>
+ *</div><!-- tab -->
+ *```
  */
-jQuery.tab = function(name) {
+jQuery.fn.tab = function( name ) {
 
     var tab_selector = name || '.tab';
-    var tab_anchors  = $('a', tab_selector + '-menu');
-    var tab_contents = $('div' + tab_selector + '-content');
+    var tab_anchors  = $( 'a', tab_selector + '-menu' );
+    var tab_contents = $( 'div' + tab_selector + '-content' );
 
     // callback of click event handler
-    function clickHandler(index) {
+    function clickHandler( i ) {
         return function() {
             var  j;
-            for (j = 0; j < tab_contents.length; j++) {
-                if (index === j) {
-                    $(tab_anchors[j]).addClass('active');
+            for ( j = 0; j < tab_contents.length; j++ ) {
+                if ( i === j ) {
+                    $( tab_anchors[ j ] ).addClass( 'active' );
                     // all tab_contents display none
-                    tab_contents.css('display', 'none');
+                    tab_contents.css( 'display', 'none' );
                     // k tab content display
-                    $(tab_contents[j]).addClass('selected');
-                    $(tab_contents[j]).css('display', 'block');
+                    $( tab_contents[ j ] ).addClass( 'selected' );
+                    $( tab_contents[ j ] ).css( 'display', 'block' );
                 } else {
-                    $(tab_anchors[j]).removeClass('active');
-                    $(tab_contents[j]).removeClass('selected');
+                    $( tab_anchors[ j ] ).removeClass( 'active' );
+                    $( tab_contents[ j ] ).removeClass( 'selected' );
                 }
             }
             return false;
@@ -29,9 +59,9 @@ jQuery.tab = function(name) {
     }
 
     // handle tab menu anchor click
-    tab_anchors.each(function(i) {
-        var handler = clickHandler(i);
-        $(tab_anchors[i]).click(handler);
+    tab_anchors.each(function( i ) {
+        var handler = clickHandler( i );
+        $( tab_anchors[ i ] ).click( handler );
     });
 
 };
